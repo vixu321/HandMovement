ad0252fa9c54f711c9f80f9e5c076e4c35d63497
vixu321 <vixu19@gmail.com>
2021-09-10 16:21:38 +0200

    Project upload

---
+++ Visual Studio Files/HandMovement/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace HandMovement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cboCamera = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboCamera
+            // 
+            this.cboCamera.FormattingEnabled = true;
+            this.cboCamera.Location = new System.Drawing.Point(61, 6);
+            this.cboCamera.Name = "cboCamera";
+            this.cboCamera.Size = new System.Drawing.Size(243, 21);
+            this.cboCamera.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Camera";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pic
+            // 
+            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic.Location = new System.Drawing.Point(15, 33);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(640, 480);
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(310, 6);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(391, 6);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(569, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "180";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 518);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboCamera);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Webcam";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboCamera;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Visual Studio Files/HandMovement/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using static System.Threading.Timer;
+using System.Timers;
+using System.IO.Ports;
+
+namespace HandMovement
+{
+    public partial class Form1 : Form
+    {
+        int timeElapsed = 0;
+
+        SerialPort port;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+
+            initServo();
+
+        }
+
+        private void initServo()
+        {
+            port = new SerialPort();
+            port.PortName = "COM5";
+            port.BaudRate = 9600;
+
+
+            try
+            {
+                port.Open();
+            }
+            catch(Exception e1)
+            {
+                Console.WriteLine(e1.Message);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeElapsed += timer1.Interval;
+            //Console.WriteLine(timeElapsed);
+        }
+
+
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Setup the camera device 
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+                cboCamera.Items.Add(filterInfo.Name);
+            cboCamera.SelectedIndex = 0;
+            videoCaptureDevice = new VideoCaptureDevice();
+            
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //Start the video feed to the picture box
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+        }
+
+        //Stop the video feed
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning == true)
+                videoCaptureDevice.Stop();
+            pic.Image = new Bitmap(100, 100);
+
+        }
+
+        //Video new frame function
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+                Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+                if(timeElapsed%100==0)
+                    screenAnalyzer(bmp);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning == true)
+                videoCaptureDevice.Stop();
+        }
+
+        
+
+
+        /* private void screenAnalyzer(Bitmap bmp)
+         {
+             bool firstInstancex = false;
+             bool lastInstancex = false;
+             System.Drawing.Graphics gr = System.Drawing.Graphics.FromImage(bmp);
+             int objectHeight=0;
+             int objectWidth=0;
+             int leftSideBuffer=0;
+             int spacesBetweenChecks = 5;
+             Point BR = new Point(0, 0);
+             Point LU = new Point(0, 0);
+
+
+
+
+             for (int i = 0; i < bmp.Height; i+=spacesBetweenChecks)
+                 {
+                     for (int j = 0; j < bmp.Width; j+=spacesBetweenChecks)
+                     {
+                         if (bmp.GetPixel(j, i).R >200 || bmp.GetPixel(j, i).G > 200 || bmp.GetPixel(j, i).B > 200)
+                         {
+                             if (firstInstancex == false)
+                             {
+
+                                LU = new Point(j, i);
+                                 firstInstancex = true;
+                             }
+
+                             if (j > objectWidth)
+                             {
+                                 BR.X = j;
+                             }
+
+                         if (i > objectHeight)
+                             {
+                                 BR.Y = i;
+                             }
+
+                                 bmp.SetPixel(j, i, Color.Red);
+                         }
+
+                     else
+                             {
+                                 leftSideBuffer++;
+                             }
+                     }
+                 }
+
+             gr.DrawLine(new Pen(Brushes.Purple, 10), new Point(LU.X,LU.Y), new Point(BR.X, LU.Y));
+             gr.DrawLine(new Pen(Brushes.Purple, 10), new Point(LU.X, LU.Y), new Point(LU.X, BR.Y));
+             gr.DrawLine(new Pen(Brushes.Purple, 10), new Point(BR.X, LU.Y), new Point(BR.X, BR.Y));
+             gr.DrawLine(new Pen(Brushes.Purple, 10), new Point(LU.X, BR.Y), new Point(BR.X, BR.Y));
+
+
+
+             objectWidth = LU.X - BR.X;
+             objectHeight = LU.Y - BR.Y;
+
+             Center = new Point(LU.X+(objectWidth/2),LU.Y+(objectHeight/2));
+
+             int centerRectangle = 10;
+
+
+
+             pic.Image = bmp;
+
+             }*/
+
+
+        Point previousPoint;
+        Point currentPoint;
+
+        private void screenAnalyzer(Bitmap bmp)
+        {
+
+            System.Drawing.Graphics gr = System.Drawing.Graphics.FromImage(bmp);
+            int spacesBetweenChecks = 5;
+            bool firstInstanceX = false;
+            bool firstInstanceY = false;
+            int firstX = 0;
+            int firstY = 0;
+
+
+
+
+
+            for (int j = 0; j < bmp.Height; j += spacesBetweenChecks)
+            {
+                for (int i = 0; i < bmp.Width; i += spacesBetweenChecks)
+                {
+                    if (bmp.GetPixel(i, j).R > 180 || bmp.GetPixel(i, j).G > 180 || bmp.GetPixel(i, j).B > 180)
+                    {
+                        if (firstInstanceX == false)
+                        {
+                            firstX = i;
+                            firstInstanceX = true;
+                           
+                            if (firstInstanceY == false)
+                            {
+                                firstY = j;
+                                firstInstanceY = true;
+
+                                currentPoint = new Point(i, j);
+                            }
+                        }
+
+                       
+
+                        
+                        bmp.SetPixel(i, j, Color.Red);
+                    }
+
+
+                }
+            }
+
+            
+            
+            gr.DrawLine(new Pen(Brushes.Purple, 10), new Point(0,0), new Point(firstX,firstY));
+            movementAnalyzer(previousPoint, currentPoint, bmp);
+            pic.Image = bmp;
+
+            previousPoint = currentPoint;
+        }
+
+
+        public void movementAnalyzer(Point previousPoint, Point currentPoint, Bitmap bmp)
+        {
+
+            System.Drawing.Graphics gr = System.Drawing.Graphics.FromImage(bmp);
+            bool upDirection=false;
+            bool rightDirection=false;
+
+            int screenWidth = bmp.Width;
+            int screenHeight = bmp.Height;
+
+            
+
+            float xDistance;
+            float yDistance;
+
+            xDistance = currentPoint.X - previousPoint.X;
+            yDistance = currentPoint.Y - previousPoint.Y;
+
+            if (xDistance < 0)
+            {
+                rightDirection = false;
+                Console.WriteLine("Left");           }
+            else if (xDistance > 0)
+            {
+                rightDirection = true;
+                Console.WriteLine("Right");
+            }
+
+            if (yDistance < 0)
+            {
+                upDirection = true;
+                Console.WriteLine("Up");
+            }
+            else if (yDistance > 0) 
+            {
+                upDirection = false;
+                Console.WriteLine("Down");
+            }
+
+            //Console.WriteLine(screenHeight + " " + screenWidth);
+
+
+            //Console.WriteLine("Previous point: " +previousPoint.X.ToString() + ", " + previousPoint.Y.ToString());
+            //Console.WriteLine("CurrentPoint: " + currentPoint.X.ToString() + ", " + currentPoint.Y.ToString());
+            //Console.WriteLine(xDistance.ToString(".0#")+ " " + yDistance.ToString(".0#"));
+
+            float xMovementFactor = xDistance / screenWidth;
+            float yMovementFactor = yDistance / screenHeight;
+
+            Console.WriteLine(xMovementFactor.ToString()+" AND " + yMovementFactor.ToString());
+
+            float positionX = (currentPoint.X * 180) / screenWidth;
+
+
+            gr.DrawLine(new Pen(Brushes.Red, 10), previousPoint, currentPoint);
+
+            if (port.IsOpen)
+            {
+                port.WriteLine(positionX.ToString());
+            }
+        }
+
+        
+        
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (port.IsOpen)
+            {
+                port.WriteLine(0.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (port.IsOpen)
+            {
+                port.WriteLine(155.ToString());
+            }
+        }
+    } 
+        
+    }
+
